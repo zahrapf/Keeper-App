5976629b26908e5633969d9cc957a12eae975382
zahrapf <zp.fakhar1996@gmail.com>
2020-04-28 22:47:18 -0400

    Finished the basic implementation of Keeper App

---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note(){
+    return (
+        <div className="note">
+            <h1>This is the title.</h1>
+            <p>This is the paragraph</p>
+        </div>
+    );
+
+}
+
+export default Note;
