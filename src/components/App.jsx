553a6974bd08edab8ce042a468f29d7a84eb7540
zahrapf <zp.fakhar1996@gmail.com>
2020-05-06 20:30:10 -0400

    Add delete functionality and change some CSS code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+//import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 
 function App() {
+
+    const [notes, setNotes] = useState([]);
+
+    function addNote(newNote){
+        setNotes(prevNotes => {
+            return [...prevNotes, newNote];
+        });
+    }
+
+    function deleteNote(id) {
+        setNotes(prevNotes => {
+            return prevNotes.filter((noteItem, index) => {
+                return index !== id;
+            });
+        });
+    }
+
     return ( 
         <div>
             <Header />
-            {notes.map(note => {
-
-                return(
+            <CreateArea onAdd={addNote}/>
+            {notes.map((noteItem, index) => {
+                return (
                     <Note 
-                        key = {note.key}
-                        title = {note.title}
-                        content = {note.content}
-                    />
-
+                        key={index}
+                        id={index}
+                        title={noteItem.title} 
+                        content={noteItem.content} 
+                        onDelete={deleteNote}/>
                 );
-            })}
-            
+               
+            })} 
             <Footer />
         </div>
     );
